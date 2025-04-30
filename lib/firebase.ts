a29cbd1f66c81cd782a4203ab45708904372e32a
q5tj <r3567089@gmail.com>
2025-04-30 14:49:07 +0300

    fix

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,17 +4,16 @@ import { getFirestore } from "firebase/firestore"
 import { getStorage } from "firebase/storage"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA-DnTdqjQW9-gq6UBEcnhIk7gT1dHdSBI",
-  authDomain: "invoice-bed82.firebaseapp.com",
-  databaseURL: "https://invoice-bed82-default-rtdb.firebaseio.com",
-  projectId: "invoice-bed82",
-  storageBucket: "invoice-bed82.firebasestorage.app",
-  messagingSenderId: "387761020229",
-  appId: "1:387761020229:web:a22e0b4058ae9fb7b0f22c",
-  measurementId: "G-SB2H7229YE",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 }
 
-// Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
 const auth = getAuth(app)
 const db = getFirestore(app)
